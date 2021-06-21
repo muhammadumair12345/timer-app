b8ede9f8e2cdc3314b2876c5141308e95b739f40
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-06-21 07:58:25 +0500

    Create TimerApp.test.tsx

---
+++ src/components/TimerApp/TimerApp.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TimerApp from './TimerApp';
+
+test('renders the component', () => {
+  const component = shallow(<TimerApp/>);
+  expect(component).toBeVisible();
+});
